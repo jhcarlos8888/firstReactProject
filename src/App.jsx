dc048da5a0d2a5aba9a75668997d2bb4f67d1ab1
Juan Carlos Herrera Blandon <jhcarlos8888@gmail.com>
2023-02-17 08:50:19 -0500

    creacion de componente basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+const MyButton = () =>{
+    return(
+        <button>Yo soy un botonsito</button>  
+    )
+};
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const App = () => {
+    const title = "Mi nombre desde una constante"
+    const centrarTexto = "text-center"
+    const coloresTitulo = {
+        titulo1: "rojito",
+        titulo2: "omesito"
+    }
 
-export default App
+    const pathImagen = "https://picsum.photos/200"
+
+    const footer = "Mi pie de pagina"
+
+    const extraccion = footer
+
+    return(
+    <>
+    <h1 className={centrarTexto}>{extraccion}</h1>
+    <img src={pathImagen} alt={coloresTitulo.titulo1}/> 
+    <MyButton></MyButton>
+    </>
+     
+    )
+       
+};
+
+export default App;
